b5a439b8d873f8010980d73951a65d307b91cc6f
Supapit <supapitploy1345@gmail.com>
2022-05-21 13:34:11 +0700

    modified file l

--- MyProject.java
+++ MyProject.java
@@ -16,6 +16,7 @@ public class MyProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World!");
     }
     
 }
